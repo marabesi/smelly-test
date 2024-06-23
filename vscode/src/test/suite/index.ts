06306855868f576fc014230d30117409878eb8f3
marabesi <matheus.marabesi@gmail.com>
2024-06-23 11:56:25 +0200

    fix: ts types executing tests

--- vscode/src/test/suite/index.ts
+++ vscode/src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import Mocha from 'mocha';
+import glob from 'glob';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -12,7 +12,7 @@ export function run(): Promise<void> {
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err: null | Error, files: any[]) => {
 			if (err) {
 				return e(err);
 			}
@@ -22,7 +22,7 @@ export function run(): Promise<void> {
 
 			try {
 				// Run the mocha test
-				mocha.run(failures => {
+				mocha.run((failures: number) => {
 					if (failures > 0) {
 						e(new Error(`${failures} tests failed.`));
 					} else {

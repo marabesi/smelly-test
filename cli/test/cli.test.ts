71f64cab4bb809219c42ff260059f8007cc1b39f
marabesi <matheus.marabesi@gmail.com>
2024-12-06 12:07:48 +0100

    chore(cli): replace tets by vitest

--- cli/test/cli.test.ts
+++ cli/test/cli.test.ts
@@ -1,14 +1,14 @@
+import { test, describe, expect } from 'vitest';
 import { exec } from "child_process";
 import { promisify } from "util";
-import * as assert from 'assert';
 
 const execPromise = promisify(exec);
 
 describe('cli', () => {
 
-  it('find no smells for a given path', async () => {
+  test('find no smells for a given path', async () => {
     const { stdout } = await execPromise(`npm run cli -- fake-data/no-smells/ javascript --report=html --report-output=$(pwd)`);
 
-    assert.equal(true, stdout.includes("Report HTML generated"));
+    expect(stdout).toContain("Report HTML generated");
   });
 });

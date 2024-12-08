ecfeab3b90f91031946632ddfb6fa7b5c89cd8a7
marabesi <matheus.marabesi@gmail.com>
2024-12-08 18:06:56 +0100

    refactor(detector): moved function calls to class

--- detector/src/smells-detector.ts
+++ detector/src/smells-detector.ts
@@ -29,16 +29,16 @@ export class SmellDetector {
     // wondering why createSource? https://stackoverflow.com/a/60462133/2258921
     const ast = ts.createSourceFile('temp.ts', this.code, ts.ScriptTarget.ES2020, true);
 
-    const testCases = findItCalls(ast).map(({ lineStart, columnStart, lineEnd, columnEnd }) => ({
+    const testCases = this.findItCalls(ast).map(({ lineStart, startAt, lineEnd, endsAt }) => ({
       lineStart,
-      startAt: columnStart,
+      startAt: startAt,
       lineEnd,
-      endsAt: columnEnd,
+      endsAt: endsAt,
     }));
 
-    const foundItEachCalls = findItEachCalls(ast);
+    const foundItEachCalls = this.findItEachCalls(ast);
     testCases.push(...foundItEachCalls);
-    testCases.push(...findItSkipCalls(ast));
+    testCases.push(...this.findItSkipCalls(ast));
 
     const smellsList = {
       fileName: this.fileName,
@@ -47,92 +47,92 @@ export class SmellDetector {
     };
     return { smellsList, testCases };
   }
-}
 
-function findItCalls(sourceFile: ts.SourceFile): { lineStart: number, columnStart: number, lineEnd: number, columnEnd: number }[] {
-  const itCalls: { lineStart: number, columnStart: number, lineEnd: number, columnEnd: number }[] = [];
+  private findItCalls(sourceFile: ts.SourceFile): { lineStart: number, startAt: number, lineEnd: number, endsAt: number }[] {
+    const itCalls: { lineStart: number, startAt: number, lineEnd: number, endsAt: number }[] = [];
 
-  function traverse(node: ts.Node) {
-    if (ts.isCallExpression(node)) {
-      const expression = node.expression;
-      const isItCall = ts.isIdentifier(expression) && expression.text === 'it';
-      const isTestCall = ts.isIdentifier(expression) && expression.text === 'test';
-      if (isItCall || isTestCall) {
-        const { line, character } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
-        const { line: lineEnd, character: columnEnd } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
-        itCalls.push({
-          lineStart: line + 1,
-          columnStart: character,
-          lineEnd: lineEnd + 1,
-          columnEnd, 
-        });
-      }
-    }
-
-    ts.forEachChild(node, traverse);
-  }
-
-  traverse(sourceFile);
-  return itCalls;
-}
-
-function findItEachCalls(sourceFile: ts.SourceFile): TestCase[] {
-  const testCases: TestCase[] = [];
-
-  function traverse(node: ts.Node) {
-    if (ts.isCallExpression(node)) {
-      const expression = node.expression;
-      const isItEachExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'it' && expression.name.text === 'each';
-      const isTestEachExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'test' && expression.name.text === 'each';
-
-      if (isItEachExpression || isTestEachExpression) {
-        const { line: startLine, character: startCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
-        const { line: endLine, character: endCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
-        const arrayArgument = node.arguments[0];
-        if (ts.isArrayLiteralExpression(arrayArgument)) {
-          arrayArgument.elements.forEach(() => {
-            testCases.push({
-              lineStart: startLine + 1,
-              startAt: startCharacter,
-              lineEnd: endLine + 1,
-              endsAt: endCharacter,
-            });
+    function traverse(node: ts.Node) {
+      if (ts.isCallExpression(node)) {
+        const expression = node.expression;
+        const isItCall = ts.isIdentifier(expression) && expression.text === 'it';
+        const isTestCall = ts.isIdentifier(expression) && expression.text === 'test';
+        if (isItCall || isTestCall) {
+          const { line, character } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
+          const { line: lineEnd, character: endsAt } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
+          itCalls.push({
+            lineStart: line + 1,
+            startAt: character,
+            lineEnd: lineEnd + 1,
+            endsAt,
           });
         }
       }
+
+      ts.forEachChild(node, traverse);
     }
 
-    ts.forEachChild(node, traverse);
+    traverse(sourceFile);
+    return itCalls;
   }
 
-  traverse(sourceFile);
-  return testCases;
-}
+  private findItEachCalls(sourceFile: ts.SourceFile): TestCase[] {
+    const testCases: TestCase[] = [];
 
-function findItSkipCalls(sourceFile: ts.SourceFile): TestCase[] {
-  const testCases: TestCase[] = [];
+    function traverse(node: ts.Node) {
+      if (ts.isCallExpression(node)) {
+        const expression = node.expression;
+        const isItEachExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'it' && expression.name.text === 'each';
+        const isTestEachExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'test' && expression.name.text === 'each';
 
-  function traverse(node: ts.Node) {
-    if (ts.isCallExpression(node)) {
-      const expression = node.expression;
-      const isItSkipExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'it' && expression.name.text === 'skip';
-      const isTestSkipExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'test' && expression.name.text === 'skip';
-
-      if (isItSkipExpression || isTestSkipExpression) {
-        const { line: startLine, character: startCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
-        const { line: endLine, character: endCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
-        testCases.push({
-          lineStart: startLine + 1,
-          startAt: startCharacter,
-          lineEnd: endLine + 1,
-          endsAt: endCharacter,
-        });
+        if (isItEachExpression || isTestEachExpression) {
+          const { line: startLine, character: startCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
+          const { line: endLine, character: endCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
+          const arrayArgument = node.arguments[0];
+          if (ts.isArrayLiteralExpression(arrayArgument)) {
+            arrayArgument.elements.forEach(() => {
+              testCases.push({
+                lineStart: startLine + 1,
+                startAt: startCharacter,
+                lineEnd: endLine + 1,
+                endsAt: endCharacter,
+              });
+            });
+          }
+        }
       }
+
+      ts.forEachChild(node, traverse);
     }
 
-    ts.forEachChild(node, traverse);
+    traverse(sourceFile);
+    return testCases;
   }
 
-  traverse(sourceFile);
-  return testCases;
+  private findItSkipCalls(sourceFile: ts.SourceFile): TestCase[] {
+    const testCases: TestCase[] = [];
+
+    function traverse(node: ts.Node) {
+      if (ts.isCallExpression(node)) {
+        const expression = node.expression;
+        const isItSkipExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'it' && expression.name.text === 'skip';
+        const isTestSkipExpression = ts.isPropertyAccessExpression(expression) && ts.isIdentifier(expression.expression) && expression.expression.text === 'test' && expression.name.text === 'skip';
+
+        if (isItSkipExpression || isTestSkipExpression) {
+          const { line: startLine, character: startCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getStart());
+          const { line: endLine, character: endCharacter } = sourceFile.getLineAndCharacterOfPosition(node.getEnd());
+          testCases.push({
+            lineStart: startLine + 1,
+            startAt: startCharacter,
+            lineEnd: endLine + 1,
+            endsAt: endCharacter,
+          });
+        }
+      }
+
+      ts.forEachChild(node, traverse);
+    }
+
+    traverse(sourceFile);
+    return testCases;
+  }
 }
